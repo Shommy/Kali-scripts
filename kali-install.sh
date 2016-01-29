502dfc5449ae7154823eab894f1cf53fbe6264a6
Milos Celic <miloscelic@gmail.com>
2016-01-29 00:02:56 -0500

    Fixed Java, added a few tools

--- kali-install.sh
+++ kali-install.sh
@@ -22,17 +22,25 @@ msfconsole -r init.rc
 
 # java:
 
-curl -L -O -H "Cookie: oraclelicense=accept-securebackup-cookie" -k http://download.oracle.com/otn-pub/java/jdk/8u66-b17/jdk-8u66-linux-x64.tar.gz
-tar xvzf jdk-8u66-linux-x64.tar.gz
-rm jdk-8u66-linux-x64.tar.gz
-mv jdk1.8.0_66 /opt/
-cd /opt/jdk1.8.0_66
-update-alternatives --install /usr/bin/java java /opt/jdk1.8.0_66/bin/java 1
-update-alternatives --install /usr/bin/javac javac /opt/jdk1.8.0_66/bin/javac 1
-update-alternatives --install /usr/lib/mozilla/plugins/libjavaplugin.so mozilla-javaplugin.so /opt/jdk1.8.0_66/jre/lib/amd64/libnpjp2.so 1
-update-alternatives --set java /opt/jdk1.8.0_66/bin/java
-update-alternatives --set javac /opt/jdk1.8.0_66/bin/javac
-update-alternatives --set mozilla-javaplugin.so /opt/jdk1.8.0_66/jre/lib/amd64/libnpjp2.so
+downloads_page='http://www.oracle.com'$(curl -i -X GET http://www.oracle.com/technetwork/java/javase/downloads/index.html 2>/dev/null | sed -n 's/.*href="\([^"]*\).*/\1/p' | grep jdk8-downloads)
+download_link=$(curl -i -X GET $downloads_page 2>/dev/null | grep 'linux-x64.tar.gz' | cut -d":" -f4,5 | cut -d'"' -f2 | tail -1)
+
+ver_major=$(echo $download_link | cut -d"/" -f7 | cut -d"u" -f1)
+ver_minor=$(echo $download_link | cut -d"/" -f7 | cut -d"u" -f2 | cut -d"-" -f1)
+
+curl -L -O -H "Cookie: oraclelicense=accept-securebackup-cookie" -k $download_link
+
+tar xvzf jdk-$ver_major'u'$ver_minor-linux-x64.tar.gz
+rm jdk-$ver_major'u'$ver_minor-linux-x64.tar.gz
+mv jdk1.$ver_major.0_$ver_minor /opt/
+cd /opt/jdk1.$ver_major.0_$ver_minor
+
+update-alternatives --install /usr/bin/java java /opt/jdk1.$ver_major.0_$ver_minor/bin/java 1
+update-alternatives --install /usr/bin/javac javac /opt/jdk1.$ver_major.0_$ver_minor/bin/javac 1
+update-alternatives --install /usr/lib/mozilla/plugins/libjavaplugin.so mozilla-javaplugin.so /opt/jdk1.$ver_major.0_$ver_minor/jre/lib/amd64/libnpjp2.so 1
+update-alternatives --set java /opt/jdk1.$ver_major.0_$ver_minor/bin/java
+update-alternatives --set javac /opt/jdk1.$ver_major.0_$ver_minor/bin/javac
+update-alternatives --set mozilla-javaplugin.so /opt/jdk1.$ver_major.0_$ver_minor/jre/lib/amd64/libnpjp2.so
 
 # veil-evasion:
 
@@ -87,6 +95,31 @@ ln -s /opt/NoSQLMap/nosqlmap.py /usr/bin/nosqlmap
 # nishang
 
 git clone https://github.com/samratashok/nishang /opt/nishang
+
+# netscan (windows tool) --> has some issues, throws exceptions
+
+wget https://www.softperfect.com/download/freeware/netscan.zip
+mkdir /opt/netscan/
+mv netscan.zip /opt/netscan/
+cd /opt/netscan/
+unzip netscan.zip
+rm netscan.zip
+echo "#!/bin/bash" > /opt/netscan/netscan_run.sh
+echo >> /opt/netscan/netscan_run.sh
+echo "wine /opt/netscan/32-bit/netscan.exe &" >> /opt/netscan/netscan_run.sh
+echo >> /opt/netscan/netscan_run.sh
+chmod +x /opt/netscan/netscan_run.sh
+ln -s /opt/netscan/netscan_run.sh /usr/bin/netscan
+
+# ApacheDirectoryStudio - LDAP browser
+
+ads_download_link=$(curl -i -k -X GET https://directory.apache.org/studio/download/download-linux.html 2>/dev/null | sed -n 's/.*href="\([^"]*\).*/\1/p' | grep -E 'x86_64.tar.gz$' | head -1)
+ads_file_name=$(echo $ads_download_link | awk -F"/" '{ print $NF }')
+wget $ads_download_link
+tar xvfz $ads_file_name
+mv ApacheDirectoryStudio /opt/
+rm $ads_file_name
+ln -s /opt/ApacheDirectoryStudio/ApacheDirectoryStudio /usr/bin/ApacheDirectoryStudio
 
 
 
